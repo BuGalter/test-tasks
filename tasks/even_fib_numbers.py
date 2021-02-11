d81f08177f2ac21d5a7d182dcc555dca44a33529
BuGalter <wardoc78@gmail.com>
2021-02-11 13:19:11 +0300

    Added license in even_fib_numbers.py

--- tasks/even_fib_numbers.py
+++ tasks/even_fib_numbers.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# Copyright [2021] [Valeriy Yukubchik]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Модуль even_fib_numbers используется для поиска положительных чисел
 Фибаначчи до переданного порядкового номера числа
 
@@ -34,8 +49,10 @@ def even_fib_numbers(number: int = 0) -> list:
 
     Parameters
     ----------
-    even number : int, positive
+    number : int, positive
         порядковый номер последнего числа интервала
+    even_elements : list
+        лист, содержит результат работы функции
 
     Tests
     -----
